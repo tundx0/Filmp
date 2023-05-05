6e7c6856af4939ce01fcdf8835eef87c879f6bb8
Olatunde Adegboyebo <tundx0@gmail.com>
2023-05-05 23:24:08 +0100

    update movies component

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { tmdbApi } from '../services/TMDB';
 
 export default configureStore({
-  reducers: {
-
+  reducer: {
+    [tmdbApi.reducerPath]: tmdbApi.reducer,
   },
 });
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Box, Circular, useMediaQuery, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { useGetMoviesQuery } from '../../services/TMDB';
 
 const Movies = () => {
-  console.log('Movies');
+  const { data } = useGetMoviesQuery();
+  console.log(data);
   return (
     <div>Movies</div>
   );
