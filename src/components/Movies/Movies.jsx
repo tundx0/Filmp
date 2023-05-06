397c11a7cc468a57c907f92e87b994fa5ee7ca21
Olatunde Adegboyebo <tundx0@gmail.com>
2023-05-06 23:57:34 +0100

    update Movies component

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { Box, Circular, useMediaQuery, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { useGetMoviesQuery } from '../../services/TMDB';
+import MovieList from '../MovieList/MovieList';
 
 const Movies = () => {
   const { data } = useGetMoviesQuery();
   console.log(data);
   return (
-    <div>Movies</div>
+    <div>
+      <MovieList movies={data} />
+    </div>
   );
 };
 
