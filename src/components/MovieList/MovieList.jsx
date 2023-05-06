84da22a34f94bba9be8c68bec818c47c69811e7b
Olatunde Adegboyebo <tundx0@gmail.com>
2023-05-06 23:58:11 +0100

    add MovieList component

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+
+const MovieList = ({ movies }) => {
+  console.log('data');
+  return (
+    <div>MovieList</div>
+  );
+};
+
+export default MovieList;
