59c08833c15279431987c29c591187148c1daa9c
Olatunde Adegboyebo <tundx0@gmail.com>
2023-05-07 01:22:36 +0100

    fix API key undefined error

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const tmdbApiKey = process.env.TMDB_KEY;
+const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
 const page = 1;
 
 export const tmdbApi = createApi({
@@ -8,7 +8,7 @@ export const tmdbApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
   endpoints: (builder) => ({
     getMovies: builder.query({
-      query: () => `movie/popular?page=${page}&api_key${tmdbApiKey}`,
+      query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
     }),
   }),
 });
