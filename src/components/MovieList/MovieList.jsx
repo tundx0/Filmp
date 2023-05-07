cb186af9e9fae3cf915692b36df6fc97449cdc4e
Olatunde Adegboyebo <tundx0@gmail.com>
2023-05-07 01:25:21 +0100

    Add movieList component and styling

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Grid } from '@mui/material';
 
+import useStyles from './styles';
+import Movie from '../Movie/Movie';
+
 const MovieList = ({ movies }) => {
-  console.log('data');
+  const classes = useStyles();
   return (
-    <div>MovieList</div>
+    <Grid container className={classes.moviesContainer}>
+      {movies.results.map((movie, i) => (
+        <Movie key={i} movie={movie} i={i} />
+      ))}
+    </Grid>
   );
 };
 
