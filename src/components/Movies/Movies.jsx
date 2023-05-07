9c816cccba48595d43001930eb881c8d413b7daf
Olatunde Adegboyebo <tundx0@gmail.com>
2023-05-07 01:26:41 +0100

    update loading logic

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,12 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Circular, useMediaQuery, Typography } from '@mui/material';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { useGetMoviesQuery } from '../../services/TMDB';
 import MovieList from '../MovieList/MovieList';
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery();
-  console.log(data);
+  const { data, isLoading, error, isFetching } = useGetMoviesQuery();
+  // console.log(data);
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No Movies that match that name
+          <br />
+          Please search for something else.
+
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return 'An error has occured';
+
   return (
     <div>
       <MovieList movies={data} />
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable comma-dangle */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Provider } from 'react-redux';
