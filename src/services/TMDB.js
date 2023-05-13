43ba27aeb7ec6b88d33503e1ff18e05761b4385d
Olatunde Adegboyebo <tundx0@gmail.com>
2023-05-13 08:42:47 +0100

    add genre API endpoint

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -7,6 +7,10 @@ export const tmdbApi = createApi({
   reducerPath: 'tmdbApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
   endpoints: (builder) => ({
+    // get genres
+    getGenres: builder.query({
+      query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
+    }),
     getMovies: builder.query({
       query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
     }),
@@ -14,5 +18,6 @@ export const tmdbApi = createApi({
 });
 
 export const {
+  useGetGenresQuery,
   useGetMoviesQuery,
 } = tmdbApi;
