585dff2a95a626a5082f64f683951f48ce27d524
Olatunde Adegboyebo <tundx0@gmail.com>
2023-09-09 23:54:45 +0100

    movie information page

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Modal, Typography, Grid, Box, CircularProgress, useMediaQuery, Rating } from '@mui/material';
+import { Movie as MovieIcon, Theaters, Language, PlusOne, Favorite, FavoriteOutlined, Remove, ArrowBack } from '@mui/icons-material';
+import { Link, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useGetMovieQuery } from '../../services/TMDB';
 
 const MovieInformation = () => {
-  console.log('MovieInformation');
+  const { id } = useParams();
+  const { data, isFetching, error } = useGetMovieQuery();
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <CircularProgress size="8rem" />
+      </Box>
+    );
+  }
+  if (error) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <Link to="/">Something has gone wrong go back</Link>
+      </Box>
+    );
+  }
   return (
-    <div>MovieInformation</div>
+    <Grid container className={classes.containerSpaceAround}>
+
+    </Grid>
   );
 };
 
