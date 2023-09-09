455b9b375c7b2cd287fbd91f2c31db1a8fc86675
Olatunde Adegboyebo <tundx0@gmail.com>
2023-09-09 23:30:56 +0100

    logout and initial profile setup

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Box, Button, Typography } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
+import { userSelector } from '../../features/auth';
 
 const Profile = () => {
-  console.log('Profile');
+  const user = useSelector(userSelector);
+  const logOut = () => {
+    localStorage.clear();
+
+    window.location.href = '/';
+  };
+  const favouriteMovies = [];
   return (
-    <div>Profile</div>
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>My Profile</Typography>
+        <Button color="inherit" onClick={logOut}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      { !favouriteMovies.length ? <Typography variant="h5">Add favourites or watchlist some movies to see them here! </Typography>
+        : (
+          <Box>
+            Favourite Movies
+          </Box>
+        )}
+    </Box>
   );
 };
 
